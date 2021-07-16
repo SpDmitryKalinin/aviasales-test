9d29042c938bd10203e929296daf58ff4de8b74a
SpDmitryKalinin <spkalinin97@gmail.com>
2021-07-16 18:29:14 +0300

    Написан класс api и принята вся информация с сервера

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,57 @@
 import React from "react";
 import Header from './Header';
 import Main from './Main';
+import api from './../utils/Api';
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      dataTickets: []
+    }
   }
+
+  componentDidMount(){
+    this.datainit();
+  }
+
   render(){
     return(
       <div className="page">
         <Header/>
-        <Main/>
+        <Main id={this.state.searchId}/>
       </div>);
+  }
+
+  datainit(){
+    this.initSearchId();
+  }
+
+  initSearchId(){
+    api.getSearchId().then(res =>{
+      this.collectData(res.searchId);
+    }); 
+  }
+
+  collectData(id){
+    api.getTicketsData(id).then(res => {
+      let collectTickets = this.state.dataTickets;
+      let fullTickets = collectTickets.concat(res.tickets);
+      this.setState({dataTickets: fullTickets});
+      if(res.stop){
+        this.show();
+        return
+      }
+      this.collectData(id);
+    })
+    .catch(res =>{
+      console.log(res);
+      this.collectData(id);
+    })
+  }
+
+  show(){
+    console.log(this.state.dataTickets); //Продолжить код
   }
 }
 
---
+++ src/utils/Api.js
@@ -0,0 +1,34 @@
+const ADDRESS = "https://front-test.beta.aviasales.ru/search";
+
+
+
+class Api{
+    constructor(addressSearchId){
+        this.address = addressSearchId;
+    }
+    getSearchId(){
+        return fetch(`https://front-test.beta.aviasales.ru/search`,{
+            method: 'GET',
+        })
+        .then(res =>{
+            return this._getResponseData(res);
+        })
+    }
+    getTicketsData(id){
+        return fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${id}`,{
+            method: 'GET',
+        })
+        .then(res =>{
+            return this._getResponseData(res);
+        })
+    }
+    _getResponseData(res) {
+        if (!res.ok) {
+            return Promise.reject(`Ошибка: ${res.status}`);
+        }
+        return res.json();
+    }
+}
+
+const api = new Api(ADDRESS);
+export default api;
